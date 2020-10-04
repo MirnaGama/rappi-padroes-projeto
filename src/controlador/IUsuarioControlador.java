236aebe6f787335f0f2848cbfa3e16d399727700
gloria-borba <gloriastefannysilvaborba@gmail.com>
2020-10-04 13:09:24 -0300

    Adicionar métodos de inserir, atualizar e procurar usuário

--- src/controlador/IUsuarioControlador.java
+++ src/controlador/IUsuarioControlador.java
@@ -1,4 +1,33 @@
 package controlador;
 
+import entidades.Transacao;
+import entidades.Usuario;
+
+import java.util.List;
+
 public interface IUsuarioControlador {
+
+    /**
+     * Inseri um usuário
+     *
+     * @param usuario
+     */
+    void inserir(Usuario usuario);
+
+    /**
+     * Atualizar o usuário
+     *
+     * @param usuario
+     * @param nome
+     * @param transacoes
+     */
+    void atualizar(Usuario usuario, String nome, List<Transacao> transacoes);
+
+    /**
+     * Procura um usuário por Cpf
+     *
+     * @param cpf
+     * @return Usuario
+     */
+    Usuario procurarPorCpf(Long cpf);
 }
--- src/controlador/impl/UsuarioControladorImpl.java
+++ src/controlador/impl/UsuarioControladorImpl.java
@@ -1,6 +1,56 @@
 package controlador.impl;
 
 import controlador.IUsuarioControlador;
+import entidades.Transacao;
+import entidades.Usuario;
+import repositorio.IUsuarioRepositorio;
+import repositorio.impl.UsuarioRepositorioImpl;
+
+import java.util.List;
 
 public class UsuarioControladorImpl implements IUsuarioControlador {
+
+    private static UsuarioControladorImpl instancia;
+    private IUsuarioRepositorio repositorioUsuario;
+
+    public static UsuarioControladorImpl getInstance() {
+        if (instancia == null) {
+            instancia = new UsuarioControladorImpl();
+        }
+        return instancia;
+    }
+
+    private UsuarioControladorImpl() {
+        repositorioUsuario = UsuarioRepositorioImpl.getInstance();
+    }
+
+    @Override
+    public void inserir(Usuario usuario) {
+        if (procurarPorCpf(usuario.getCpf()) != null) {
+            System.out.println("Usuário já existente!");
+        } else {
+            repositorioUsuario.inserir(usuario);
+            System.out.println("Usuário cadastrado com sucesso!");
+        }
+    }
+
+    @Override
+    public void atualizar(Usuario usuario, String nome, List<Transacao> transacoes) {
+        if (nome != null) {
+            repositorioUsuario.atualizar(usuario, nome, null);
+        } else if (transacoes != null) {
+            repositorioUsuario.atualizar(usuario, null, transacoes);
+        }
+    }
+
+    @Override
+    public Usuario procurarPorCpf(Long cpf) {
+        Usuario usuario = procurarPorCpf(cpf);
+        if (usuario != null) {
+            return usuario;
+        } else {
+            System.out.println("Usuário inexistente!");
+            return null;
+        }
+    }
 }
