b3db8db79ca6c2e43839fe4dcdc78872c22230c2
gloria-borba <gloriastefannysilvaborba@gmail.com>
2020-10-04 12:40:07 -0300

    Adicionar métodos de listar e inserir transacao no repositório

--- src/repositorio/ITransacaoRepositorio.java
+++ src/repositorio/ITransacaoRepositorio.java
@@ -1,7 +1,27 @@
 package repositorio;
 
+import entidades.Transacao;
+import entidades.Usuario;
+
+import java.util.List;
+
 /**
  * Interface do repositório de transacao
  */
 public interface ITransacaoRepositorio {
+
+    /**
+     * Insere uma transação
+     *
+     * @param transacao
+     */
+    void inserir(Transacao transacao, Usuario usuario);
+
+    /**
+     * Lista todas as transações do usuário
+     *
+     * @return List<Transacao>
+     */
+    List<Transacao> listar(Usuario usuario);
+
 }
